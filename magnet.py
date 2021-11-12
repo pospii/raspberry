ef261631847b0442aa14eaa269f55c570b3cd279
Adam Pospíšek <90259811+pospii@users.noreply.github.com>
2021-11-12 21:06:02 +0100

    Upozornění emailem na otevřené/zavřené dveře

--- magnet.py
+++ magnet.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+import smtplib
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 a = 0
@@ -6,8 +8,17 @@ while True:
         if GPIO.input(8) and a != 1:
             print("open")
             a = 1
+            server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+            server.login("adam.pospisek@gmail.com", "********")
+            server.sendmail("váš gmail/gmail odesílatele", "gmail příjemce", "otevřeno")
+            server.quit()
+
 
 
         elif GPIO.input(8) == False and a != 2:
               print("close")
               a = 2
+              server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+              server.login("adam.pospisek@gmail.com", "********")
+              server.sendmail("adam.pospisek@gmail.com", "adam.pospisek@email.cz", "zavřeno")
+              server.quit()
